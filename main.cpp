0acd74889af3a1c19ebcd425b8393cdbea93242a
MimoriSeica <zerons_0612@yahoo.co.jp>
2019-07-30 01:10:56 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,n) for(ll (i) = (0);(i) < (n);++i)
+#define REV(i,n) for(ll (i) = (n) - 1;(i) >= 0;--i)
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define FI first
+#define SE second
+#define SHOW1d(v,n) {REP(WW,n)cerr << v[WW] << ' ';cerr << endl << endl;}
+#define SHOW2d(v,WW,HH) {REP(W_,WW){REP(H_,HH)cerr << v[W_][H_] << ' ';cerr << endl;}cerr << endl;}
+#define ALL(v) v.begin(),v.end()
+#define Decimal fixed<<setprecision(20)
+#define INF 1000000000
+#define LLINF 1000000000000000000LL
+#define MOD 998244353
+
+typedef long long ll;
+typedef pair<ll,ll> P;
+
+int main(){
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
+
+	return 0;
+}
